72ba833858e253defd840c74b104fac080297b41
mk162707 <maanmk58@gmail.com>
2024-07-10 09:24:26 +0530

    Add files via upload

---
+++ forloop_fstream.py
@@ -0,0 +1,22 @@
+# a=10
+# b=20
+# print("sum is ", a+b)
+# print(f"sum is {a+b}")
+# #n * i =n
+# n=10
+# print(n,"*", i, "=", n)
+# print(f"{n} * {i} = {n}")
+
+
+#                  0    10 ,  1
+#for i in range(start, stop, step ):
+# for i in range(10):
+#     print(i)
+# for i in range(5,10,2):
+#     print(i)
+# for i in range(10,-2,-1):
+#     print(i)
+names = ["Eknoor", "Gurpal", "Armaan"]
+salaries = [10000,12000,13000]
+for i in range(3):
+    print(f"Name: {names[i]},  Salary: {salaries[i]}")
